3b1c98877a858d64ae19c385c09b4634967d5c2d
caiomcg <caiomcg@gmail.com>
2018-06-06 22:07:14 -0300

    Add matrix utility functions

--- src/IM/Utils.java
+++ src/IM/Utils.java
@@ -4,6 +4,7 @@ import com.sun.istack.internal.Nullable;
 import javafx.stage.FileChooser;
 import javafx.stage.Window;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 
 public class Utils {
@@ -23,5 +24,35 @@ public class Utils {
             return "";
         }
         return url.substring(index + 1);
+    }
+    public static int[][] getMatrix(BufferedImage bufferedImage) {
+        int[][] matrix = new int[bufferedImage.getWidth()][bufferedImage.getHeight()];
+
+        for (int i = 0; i < bufferedImage.getWidth(); i++) {
+            for (int j = 0; j < bufferedImage.getHeight(); j++) {
+                matrix[i][j] = bufferedImage.getRGB(i,j) & 0xFF;
+            }
+        }
+
+        return matrix;
+    }
+
+    public static BufferedImage setImage(int[][] matrix) {
+        BufferedImage image = new BufferedImage(matrix.length, matrix.length, BufferedImage.TYPE_4BYTE_ABGR);
+        for (int i = 0; i < image.getWidth(); i++) {
+            for (int j = 0; j < image.getHeight(); j++) {
+                image.setRGB(i, j, ((0xFF) << 24) | (matrix[i][j] << 16) | (matrix[i][j] << 8) | matrix[i][j]);
+            }
+        }
+        return image;
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print("" + matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
